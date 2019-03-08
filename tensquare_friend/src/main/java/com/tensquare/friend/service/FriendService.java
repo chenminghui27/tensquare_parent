e46c29b34a4af1326db9dcf0aa5e8ed795dd86de
chenminghui27 <727308517@qq.com>
2019-03-08 11:55:13 +0800

    增加打印111

--- tensquare_friend/src/main/java/com/tensquare/friend/service/FriendService.java
+++ tensquare_friend/src/main/java/com/tensquare/friend/service/FriendService.java
@@ -44,6 +44,7 @@ public class FriendService {
         }
         userClient.incFanscount(friendid,1);//增加粉丝数
         userClient.incFollowcount(userid,1);//增加关注数 11111 22222
+        System.out.println("111");
         return 1;
     }
 
